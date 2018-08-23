eab4c2a8c2de4cc3290f69775dab9bb6b52fc034
wntitus <wesn.titus@gmail.com>
2018-08-22 20:23:26 -0400

    adding package.json

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,5 +1,22 @@
 // creating our friend data array
-let friendData = [];
+let friendData = [
+    {
+        'name' : 'Wes',
+        'photo' : 'https://imgur.com/a/2bDwzv8',
+        'scores' : [
+            3,
+            2,
+            3,
+            1,
+            3,
+            4,
+            2,
+            4,
+            5,
+            5
+        ]
+    }
+];
 
 // exporting the array for use in our routes
 module.exports = friendData;
