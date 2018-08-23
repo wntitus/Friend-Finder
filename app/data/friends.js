fb3ec1868abf877d754ccdb480de556422b03f48
wntitus <wesn.titus@gmail.com>
2018-08-23 12:29:26 -0400

    adding some more friend data

--- app/data/friends.js
+++ app/data/friends.js
@@ -15,6 +15,54 @@ let friendData = [
             "5",
             "5"
         ]
+    },
+    {
+        'name' : 'Geoffrey',
+        'photo' : 'https://pbs.twimg.com/profile_images/751150850391900160/N3yDXwJ5.jpg',
+        'scores' : [
+            "5",
+            "3",
+            "2",
+            "3",
+            "4",
+            "2",
+            "3",
+            "2",
+            "4",
+            "5"
+        ]
+    },
+    {
+        'name' : 'Guy',
+        'photo' : 'https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fcdn-image.foodandwine.com%2Fsites%2Fdefault%2Ffiles%2F.%2Ffwx-kitchen-trash-guy-fieri.jpg&w=700&q=85',
+        'scores' : [
+            "5",
+            "2",
+            "5",
+            "4",
+            "3",
+            "2",
+            "3",
+            "2",
+            "2",
+            "2"
+        ]
+    },
+    {
+        'name' : 'Ree',
+        'photo' : 'https://pbs.twimg.com/profile_images/989486563758354432/2NBOn09O_400x400.jpg',
+        'scores' : [
+            "2",
+            "2",
+            "3",
+            "4",
+            "1",
+            "2",
+            "2",
+            "3",
+            "1",
+            "5"
+        ]
     }
 ];
 
